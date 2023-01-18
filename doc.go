fdc6efa738a612570dc911197c6e26aee6477060
James Dunne <james.jdunne@gmail.com>
2023-01-18 00:21:54 -0600

    sexp: add nil, bool atoms; allow negative integers; limit hexadecimal to lowercase only

--- doc.go
+++ doc.go
@@ -1,90 +1,153 @@
+// Package sexp
+//
 // s-expressions encoder and decoder (custom format)
-// author: jsd1982
-// date:   2023-01-07
 //
-// this s-expression encoding adheres to the following restrictions:
-//   1. newline-related whitespace characters ('\r', '\n') MUST NOT appear in the
-//      serialized form of an S-expression
-//   2. 8-bit clean characters. only the standard 7-bit ASCII characters have special meaning.
-//      all other characters are passed through verbatim.
+//	author: jsd1982
+//	date:   2023-01-07
 //
-// examples:
+// an s-expression is recursively either an atom expression or a list of s-expressions surrounded by '(' ')'.
 //
-//   (test-exp abc d.e.f/gh snake_case #616263# ^3#616263# ^$a#00 01 02 03 04 05 06 07 08 09 0a# 1023 $3ff "abc\ndef\t\"123\"" () (1 3))
+// encoding restrictions:
+//  1. newline whitespace characters ('\r', '\n') MUST NOT appear in an encoded s-expression.
+//  2. encoding is ASCII and 8-bit clean with exception of '\r' and '\n'.
+//  3. each atom has a single encoding with no stylistic variation so as to simplify encoding and decoding logic.
 //
-// token:
-//   alpha-numeric identifier must start with alpha char
-//   may contain digits and special punctuation chars
+// s-expression examples:
 //
-//   examples:
-//     `test-exp`
-//     `abc`
-//     `d.e.f/gh`
-//     `snake_case`
+//	(test-exp abc d.e.f/gh snake_case nil true false #616263# ^3#616263#)
+//	(^$a#00 01 02 03 04 05 06 07 08 09 0a# 1023 $3ff)
+//	("abc\ndef\t\"123\"\x00\xff" () ^5"12345")
+//	((a 1) (b 2) (c 3) (d nil) (e false))
 //
-// integer:
-//   base-10 or base-16 integer value
-//   base-10 is the default base
-//   base-16 indicated by leading '$'
+// atom types:
 //
-//   examples:
-//     `1023`
-//     `$3ff`
+//	nil
+//	bool
+//	integer
+//	token
+//	hex-string
+//	quoted-string
+//	list
 //
-// hex-string:
-//   leading '#' and trailing '#' with hex-digits and white-space in between
-//   octets are encoded as pairs of 2 hex digits regardless of white-space in between
-//   white-space is ignored and is not interpreted as an octet separator
-//   if an odd number of hex-digits is encountered, the right-most digit is assumed to be a '0'
+// nil atom type:
 //
-//   examples:
-//     `#616263#`        = #61 62 63#
-//     `#0 0102 2#`      = #00 10 22#
+//	literal "nil" keyword
 //
-// length-prefixed hex-string:
-//   leading '^' indicates length follows as an <integer> followed by the <hex-string>
-//   length can be decimal or hexadecimal
+// bool atom type:
 //
-//   examples:
-//     `^3#616263#`
-//     `^$a#00 01 02 03 04 05 06 07 08 09 0a#`
+//	literal "true" and "false" keywords
 //
-// quoted-string:
-//   leading '"' and trailing '"'
+// integer atom type:
+//
+//	a base-10 or base-16 integer value of arbitrary length
+//	may start with optional '-' to indicate negative value
+//	integers contain only allowable digit characters depending on the base
+//	no extra formatting-related ('_'), division (','), or white-space characters are allowed
+//	any number of leading zeros are allowed and *do not* signify base-8
+//	the default base is 10
+//	base-10 integer must contain only digits '0'..'9'
+//	base-16 integer must start with '$'
+//	base-16 integer must contain only digits '0'..'9','a'..'f'
+//	base-16 integer cannot contain upper-case 'A'..'F' so as to simplify encoding and decoding logic
+//
+//	examples:
+//		  `-1024`  = -1024
+//		`0001023`  =  1023
+//		   `1023`  =  1023
+//		   `$3ff`  =  1023
+//
+// token atom type:
+//
+//	alpha-numeric identifier of arbitrary length without white-space
+//	may begin with one optional '@' to escape reserved keywords like "nil", "true", "false"
+//	cannot start with a decimal digit
+//	may contain alpha characters 'a' .. 'z', 'A' .. 'Z'
+//	may contain special punctuation chars "_" | "." | "/" | "?" | "!"
+//	may contain non-ASCII characters 128 <= char <= 255
+//	may contain decimal digits '0' .. '9'
+//
+//	examples:
+//	  `test_exp`
+//	  `abc`
+//	  `d.e.f/gh`
+//	  `snake_case`
+//	  `@true`
+//	  `@nil`
+//
+// hex-string atom type:
+//
+//	optionally starts with a leading '^' followed by <integer> to specify the decoded data length
+//	leading '#' and trailing '#'
+//	encodes an octet-string with each octet described in hexadecimal with 2*n hex-digits (n >= 0)
+//	only hex-digits may appear between leading '#' and trailing '#'
+//	no white-space or other characters allowed to simplify encoding and decoding logic
+//	octets are encoded as 2 hex digits in sequence, most significant digit first followed by least significant
+//	if an odd number of hex-digits is encountered, the last digit is assumed to be the most-significant digit
+//	of a 2-digit octet and the least-significant digit is assumed to be 0.
+//
+//	examples:
+//	  `#616263#`
+//	  `#001022#`
+//	  `^3#616263#`
+//	  `^$a#0102030405060708090a#`
+//
+// quoted-string atom type:
+//
+//	optionally starts with a leading '^' followed by <integer> to specify the decoded data length
+//	leading '"' and trailing '"'
+//	may contain any ASCII and non-ASCII character except '\' and '"'
+//	a '\' is treated as the start of an escape sequence followed by one of:
+//		'\' = '\\'
+//		'"' = '\"'
+//		'r' = '\r'
+//		'n' = '\n'
+//		't' = '\t'
+//		'x' <hex-digit> <hex-digit> = escape of 8-bit character encoded in hexadecimal
+//
+//	examples:
+//	  "abc\ndef\t\"123\"\x00\xff"
+//	  ^5"12345"
 //
 // BNF:
-//  <sexpr>           :: <token> | <string> | <integer> | <list> ;
 //
-//  <list>            :: "(" ( <sexpr> | <whitespace> )* ")" ;
+//	<sexpr>           :: <nil> | <bool> | <integer> | <token> | <string> | <list> ;
 //
-//  <string>          :: ( "^" <integer> )? <data-string> ;
-//  <data-string>     :: <hex-string> | <quoted-string> ;
+//	<list>            :: "(" ( <sexpr> | <whitespace> )* ")" ;
 //
-//  <hex-string>      :: "#" ( <hex-digit> | <whitespace> )* "#" ;
+//	<nil>             :: "n" "i" "l" ;
 //
-//  <quoted-string>   :: "\"" ( <quoted-char> | <quoted-escape> )* "\"" ;
-//  <quoted-char>     :: [32 <= char <= 255, except "\"", "\\"] ;
-//  <quoted-escape>   :: "\\" ( <escape-single> | <escape-hex> ) ;
-//  <escape-single>   :: "\\" | "\"" | "n" | "r" | "t" ;
-//  <escape-hex>      :: "x" <hex-digit> <hex-digit> ;
+//	<bool>            :: <bool-true> | <bool-false> ;
+//	<bool-true>       :: "t" "r" "u" "e" ;
+//	<bool-false>      :: "f" "a" "l" "s" "e" ;
 //
-//  <integer>         :: <decimal> | <hexadecimal> ;
+//	<integer>         :: <decimal> | <hexadecimal> ;
 //
-//  <decimal>         :: <decimal-digit>+ ;
-//  <decimal-digit>   :: "0" | ... | "9" ;
+//	<decimal>         :: <decimal-digit>+ ;
+//	<decimal-digit>   :: "0" | ... | "9" ;
 //
-//  <hexadecimal>     :: "$" <hex-digit>+ ;
-//  <hex-digit>       :: <decimal-digit> | "A" | ... | "F" | "a" | ... | "f" ;
+//	<hexadecimal>     :: "$" <hex-digit>+ ;
+//	<hex-digit>       :: <decimal-digit> | "a" | ... | "f" ;
 //
-//  <whitespace>      :: <whitespace-char>* ;
-//  <whitespace-char> :: " " | "\t" ;
+//	<string>          :: ( "^" <integer> )? <data-string> ;
+//	<data-string>     :: <hex-string> | <quoted-string> ;
 //
-//  <token>           :: <token-start> <token-char>* ;
-//  <token-start>     :: <alpha> | <simple-punc> ;
-//  <token-char>      :: <token-start> | <decimal-digit> ;
-//  <alpha>           :: <upper-case> | <lower-case> ;
-//  <lower-case>      :: "a" | ... | "z" ;
-//  <upper-case>      :: "A" | ... | "Z" ;
-//  <simple-punc>     :: "-" | "." | "/" | "_" ;
-
+//	<hex-string>      :: "#" <hex-digit>* "#" ;
+//
+//	<quoted-string>   :: "\"" ( <quoted-char> | <quoted-escape> )* "\"" ;
+//	<quoted-char>     :: [any 8-bit char except "\"", "\\", "\r", "\n"] ;
+//	<quoted-escape>   :: "\\" ( <escape-single> | <escape-hex> ) ;
+//	<escape-single>   :: "\\" | "\"" | "n" | "r" | "t" ;
+//	<escape-hex>      :: "x" <hex-digit> <hex-digit> ;
+//
+//	<whitespace>      :: <whitespace-char>* ;
+//	<whitespace-char> :: " " | "\t" ;
+//
+//	<token>           :: ( "@" )? <token-start> <token-remainder>* ;
+//	<token-start>     :: <alpha> | <simple-punc> | <non-ascii> ;
+//	<token-remainder> :: <token-start> | <decimal-digit> ;
+//	<alpha>           :: <upper-case> | <lower-case> ;
+//	<lower-case>      :: "a" | ... | "z" ;
+//	<upper-case>      :: "A" | ... | "Z" ;
+//	<simple-punc>     :: "_" | "." | "/" | "?" | "!" ;
+//	<non-ascii>       :: [128 <= char <= 255] ;
 package sexp
